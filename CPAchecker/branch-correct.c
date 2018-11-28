d853ae41ea8809bb69826f0bbe11aaf73961783d
Rushi Shah <rshah@sec-research-2018.localdomain>
2018-11-28 22:36:39 +0000

    Got CPAchecker working on basic example

---
+++ CPAchecker/branch-correct.c
@@ -0,0 +1,21 @@
+extern int f();
+
+int main() {
+	int x1;
+	int x2;
+	if(f()) {
+		x1 = 1;
+		x2 = 1;	
+	} 
+	else {
+		x1 = 2;
+		x2 = 2;
+	}
+	if(x1 != x2) {
+		goto ERROR;
+	}
+	return (0);
+  ERROR:
+  return (-1);
+}
+
---
+++ CPAchecker/run.sh
@@ -0,0 +1,2 @@
+# Ex: ./run.sh branch-correct.c
+~/CPAchecker-1.6.1-unix/scripts/cpa.sh -predicateAnalysis $1
